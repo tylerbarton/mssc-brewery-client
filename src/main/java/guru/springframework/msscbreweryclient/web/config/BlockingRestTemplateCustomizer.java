2825c183925728e94053dbcc2f92e90f379cd21e
tylerbarton <tylervbarton@gmail.com>
2022-06-13 10:49:37 -0500

    Document Blocking and Non-Blocking IO template customizers

--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -13,21 +13,29 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by jt on 2019-08-08.
+ *
+ * Blocking IO
  */
-@Component
+@Component // Comment this out to get the default
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
+    /**
+     * Sets up Apache-specific client information
+     * @return
+     */
     public ClientHttpRequestFactory clientHttpRequestFactory(){
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
         connectionManager.setMaxTotal(100);
         connectionManager.setDefaultMaxPerRoute(20);
 
+        // Set custom request configuration
         RequestConfig requestConfig = RequestConfig
                 .custom()
                 .setConnectionRequestTimeout(3000)
                 .setSocketTimeout(3000)
                 .build();
 
+        // Simple closable HTTP Client
         CloseableHttpClient httpClient = HttpClients
                 .custom()
                 .setConnectionManager(connectionManager)
@@ -35,9 +43,14 @@ public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
                 .setDefaultRequestConfig(requestConfig)
                 .build();
 
+        // Return a new instance of the client
         return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 
+    /**
+     * Spring configured to use Apache-flavored HTTP client
+     * @param restTemplate
+     */
     @Override
     public void customize(RestTemplate restTemplate) {
         restTemplate.setRequestFactory(this.clientHttpRequestFactory());
--- src/main/java/guru/springframework/msscbreweryclient/web/config/NIORestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/NIORestTemplateCustomizer.java
@@ -13,10 +13,17 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by jt on 2019-08-07.
+ *
+ * Non-Blocking IO template: Sets of sockets can be used by a single thread
  */
-//@Component
+//@Component // Uncomment this to use this
 public class NIORestTemplateCustomizer implements RestTemplateCustomizer {
 
+    /**
+     * Apache configuration
+     * @return
+     * @throws IOReactorException
+     */
     public ClientHttpRequestFactory clientHttpRequestFactory() throws IOReactorException {
         final DefaultConnectingIOReactor ioreactor = new DefaultConnectingIOReactor(IOReactorConfig.custom().
                 setConnectTimeout(3000).
@@ -32,13 +39,18 @@ public class NIORestTemplateCustomizer implements RestTemplateCustomizer {
                 .setConnectionManager(connectionManager)
                 .build();
 
+        // Deprecated in Spring 5 - probably to favor reactive client
         return new HttpComponentsAsyncClientHttpRequestFactory(httpAsyncClient);
-
     }
 
+    /**
+     * Spring customization
+     * @param restTemplate
+     */
     @Override
     public void customize(RestTemplate restTemplate) {
         try {
+            // Sets up requestFactory with non-blocking IO
             restTemplate.setRequestFactory(clientHttpRequestFactory());
         } catch (IOReactorException e) {
             e.printStackTrace();
